998395aa67f8c1659f2ac1502b900b54f4f96e7b
Julius Becker <julix.becker@gmail.com>
2026-06-08 15:45:57 +0200

    Delete: per-request "facebook api usage" info log
    It logged on every response with non-zero usage, spamming logs even at
    trivial usage levels.

--- fb/transport_rate_limit.go
+++ fb/transport_rate_limit.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/go-kit/log"
-	"github.com/go-kit/log/level"
 )
 
 type rateLimitTransport struct {
@@ -34,20 +33,6 @@ func (t *rateLimitTransport) RoundTrip(r *http.Request) (*http.Response, error) 
 	resp, err := t.next.RoundTrip(r)
 
 	t.state.updateFromResponse(resp)
-
-	if resp != nil {
-		t.state.mu.Lock()
-		pct := t.state.maxUsagePct
-		t.state.mu.Unlock()
-
-		if pct > 0 {
-			_ = level.Info(t.l).Log(
-				"msg", "facebook api usage",
-				"usage_pct", pct,
-				"block_duration", t.state.blockDurationForRetry(),
-			)
-		}
-	}
 
 	return resp, err
 }
