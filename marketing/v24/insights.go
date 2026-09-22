907c3ed619dd99b1420fdcd43f1d07734d3f2476
conorbln <81810882+conorbln@users.noreply.github.com>
2026-09-22 20:58:10 +0200

    Add action_values to insights

--- marketing/v24/insights.go
+++ marketing/v24/insights.go
@@ -240,6 +240,7 @@ func (ir *InsightsRequest) generateReportDirect(ctx context.Context, c chan<- In
 type Insight struct {
 	AccountID                        string                 `json:"account_id"`
 	Actions                          ActionTypeValue        `json:"actions"`
+	ActionValues                     ActionTypeValue        `json:"action_values"`
 	AdsetID                          string                 `json:"adset_id"`
 	AdID                             string                 `json:"ad_id"`
 	Objective                        string                 `json:"objective"`
